992e9b9334f35fb4e3ae6835f23d85c59d5909a9
yun xu <yunxu@yuns-MacBook-Air.local>
2026-03-27 12:45:37 -0400

    add error message for jwt token

---
+++ demo/src/main/java/com/example/demo/exception/TokenExpiredException.java
@@ -0,0 +1,19 @@
+package com.example.demo.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.CONFLICT)
+public class TokenExpiredException extends RuntimeException {
+	
+
+	private static final long serialVersionUID = -7185829468407135384L;
+
+	public TokenExpiredException(String message) {
+		super(message);
+	}
+
+	public TokenExpiredException(String message, Throwable cause) {
+		super(message, cause);
+	}
+}
