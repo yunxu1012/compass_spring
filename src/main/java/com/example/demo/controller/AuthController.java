c19f704abde5de8f13210697cb72637f5fed0143
yun xu <yunxu@yuns-MacBook-Air.local>
2026-08-26 14:51:49 -0400

    change for aws

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -192,7 +192,7 @@ public class AuthController {
 	
 	@GetMapping(path = "/auth/Ok")
 	public ResponseEntity<String> ok() {
-		return new ResponseEntity<>("{\"status\": \"OK 010\"}", HttpStatus.OK);
+		return new ResponseEntity<>("{\"status\": \"OK 011\"}", HttpStatus.OK);
 	}
 
 }
