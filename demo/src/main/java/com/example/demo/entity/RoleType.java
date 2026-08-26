de596c17a6fbde666457632e47d392febaaccaaf
yun xu <yunxu@yuns-MacBook-Air.local>
2026-08-26 14:50:30 -0400

    delete old folder

--- demo/src/main/java/com/example/demo/entity/RoleType.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo.entity;
-
-public enum RoleType {
-   CUSTOMER,
-   ADMIN
-}
--- demo/src/main/java/com/example/demo/repository/CustomerPreferenceRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.entity.CustomerPreference;
-
-public interface CustomerPreferenceRepository extends JpaRepository<CustomerPreference, Integer> {
-}
